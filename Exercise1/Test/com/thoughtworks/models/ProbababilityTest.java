92664523563b1b86e223de23eb42a9f60d0644b8
mallikarjuna <mallikarjuna@inblr-mallikarjuna.local>
2016-05-31 10:12:39 +0530

    Probability or operation

--- Exercise1/Test/com/thoughtworks/models/ProbababilityTest.java
+++ Exercise1/Test/com/thoughtworks/models/ProbababilityTest.java
@@ -11,12 +11,27 @@ import static org.junit.Assert.*;
  */
 public class ProbababilityTest {
     @Test
-    public void testNegation() throws Exception {
+    public void testNegation() {
         assertThat(new Probability(0.8).negate(),is(new Probability(0.2)));
     }
 
+
     @Test
-    public void testEquality() throws Exception {
+    public void testAnd()  {
+        assertThat(new Probability(0.5).and(new Probability(0.5)),is(new Probability(0.25)));
+    }
+
+
+
+    @Test
+    public void testOr() {
+        assertThat(new Probability(0.5).or(new Probability(0.5)),is(new Probability(0.75)));
+        assertThat(new Probability(1).or(new Probability(1)),is(new Probability(1)));
+        assertThat(new Probability(0).or(new Probability(1)),is(new Probability(1)));
+    }
+
+    @Test
+    public void testEquality() {
 
         Probability p1=new Probability(0.2);
         assertThat(new Probability(0.2),is(p1));
--- Exercise1/src/com/thoughtworks/models/Probability.java
+++ Exercise1/src/com/thoughtworks/models/Probability.java
@@ -1,24 +1,29 @@
 package com.thoughtworks.models;
 
+import java.nio.file.ProviderNotFoundException;
+
 /**
  * Created by mallikarjuna on 5/30/16.
  */
 public class Probability {
 
 
-    double probabilityValue;
+    double chance;
 
     public Probability(double v) {
-    this.probabilityValue=v;
+    this.chance =v;
     }
 
     public Probability negate() {
-        return new Probability(1.0 - probabilityValue);
+        return new Probability(1.0 - chance);
     }
 
-    public double getProbabliltyValue(){
-        return probabilityValue;
+
+    public Probability and(Probability probability) {
+
+        return new Probability(chance * probability.chance);
     }
+
 
     @Override
     public boolean equals(Object o) {
@@ -27,7 +32,7 @@ public class Probability {
 
         Probability that = (Probability) o;
 
-        if (Double.compare(Math.round(that.probabilityValue),Math.round(probabilityValue)) !=  0) return false;
+        if (Double.compare(Math.round(that.chance),Math.round(chance)) !=  0) return false;
 
         return true;
     }
@@ -36,5 +41,13 @@ public class Probability {
     public int hashCode() {
 
         return 1;
+    }
+
+
+    public Probability or(Probability that) {
+
+        double probSum=this.chance+that.chance;
+        Probability result=new Probability(probSum - and(that).chance);
+        return result;
     }
 }
